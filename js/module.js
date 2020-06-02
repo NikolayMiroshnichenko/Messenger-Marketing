0b5ea8b8b898c505c27f50446b74f73117887c11
NikolayMiroshnichenko <36457433+NikolayMiroshnichenko@users.noreply.github.com>
2020-06-02 19:22:39 +0300

    second

--- js/module.js
+++ js/module.js
@@ -1,4 +1,5 @@
 var tabs = new Tabby('[data-tabs]');
+const firstSections = document.querySelectorAll('.first-sections');
 
 
 const moduleContainer = document.querySelector('.module');
@@ -8,6 +9,9 @@ moduleContainer.addEventListener('click', moduleActivLink);
 
 
 function moduleActivLink(e) {
+    e.preventDefault();
+    let children; 
+
     links.forEach(link => {
         let isActiv = link.getAttribute('aria-selected');
 
@@ -17,8 +21,19 @@ function moduleActivLink(e) {
             link.classList.remove('module-link-activ');
         }
         
-    })
+    });
+
+    firstSections.forEach(item => {
+        
+        // console.log(item.id);
+        if(e.target.dataset.number == item.id) {
+
+            childrens = item.children; 
+            childrens[1].classList.remove('hidden');
+            item.classList.add('box-item-activ');
+            childrens[0].children[2].classList.add('box-item-link-icon-activ');
+        }       
+    });
 }
 
 
-console.log(moduleContainer);
